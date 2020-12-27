f48c83cd6416bf76c8487252f1149266b58d82a0
Louai Bouzaher <58881632+louaibouzaher@users.noreply.github.com>
2020-12-27 16:09:49 +0100

    style

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -15,20 +15,20 @@ const Home = ({ names, setNames }) => {
   return (
     <div className="relative App w-full h-screen flex flex-col justify-start items-center py-10">
       <img src={logo} alt="logo" className="w-1/5" />
-      <div className="flex flex-row justify-center items-center w-full">
+      <div className="flex flex-row justify-center items-center w-full ">
         <input
           type="file"
           accept=".txt"
           onChange={(e) => handleFile(e)}
-          className="custom-file-input pl-16 rounded-lg blueBg shadow-2xl"
+          className="custom-file-input pl-14 p-2 rounded-lg blueBg shadow-2xl h-12 text-white"
         />
       </div>
       {names.length > 0 && (
-        <div className="scroll flex flex-col justify-center items-center mt-6 w-full h-2/3 overflow-y-scroll">
+        <div className="scroll flex flex-col justify-start items-center p-6 pb-12 w-full h-2/3 overflow-y-scroll">
           {names.map((n, idx) => (
             <div
               key={idx}
-              className="w-14 text-lg text-white text-center py-2 shadow-2xl"
+              className="text-lg text-white text-center shadow-2xl py-2"
               style={{
                 color:
                   idx % 3 === 0
@@ -53,7 +53,7 @@ const Home = ({ names, setNames }) => {
       {names.length > 0 && (
         <Link
           to="/winner"
-          className="cursor-pointer absolute text-white blueBg py-2 px-5 text-2xl rounded-3xl bottom-10 shadow-2xl"
+          className="cursor-pointer absolute text-white blueBg py-2 px-8 text-2xl rounded-3xl bottom-10 shadow-2xl"
         >
           Pick a Winner 🎉
         </Link>
